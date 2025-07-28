665e83492131de482c9212ab01bd05c2743d0b7d
Subhaprakash <Subhaprakashjana@gmail.com>
2025-07-28 16:43:43 +0530

    Services Communication Establised

---
+++ Payment_Service/Payment_Service_App.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_cors import CORS
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+CORS(app)
+metrics = PrometheusMetrics(app)
+
+@app.route('/payment', methods=['POST'])
+@app.route('/api/payment', methods=['POST'])
+def payment():
+    data = request.get_json()
+    amount = data.get("amount", 0)
+    return f"Please Proceed With The Payment!!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003)
